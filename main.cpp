1b06dba7101abe669a882dc297ba37f82c2a5e2a
Peter Li <lipet2k@Peters-MBP.fios-router.home>
2024-05-04 11:16:28 -0400

    sieve of eratosthenes

--- main.cpp
+++ main.cpp
@@ -6,6 +6,25 @@
 
 using namespace std;
 
+/**
+ * Sieve of Eratosthenes 
+ * Time Complexity: O(n log log n)
+ * Memory: O(n)
+*/
+vector<bool> sieve(int n) {
+    vector<bool> primes(n+1, true);
+    primes[0] = primes[1] = false;
+
+    for (int i = 2; i*i <= n; ++i) {
+        if (primes[i] && (long long)i*i <= n) {
+            for (int j = i*i; j <= n; j += i) {
+                primes[j] = false;
+            }
+        }
+    }
+    return primes;
+}
+
 void solve() {
 
 }
@@ -18,3 +37,10 @@ int main() {
 
     return 0;
 }
+
+/**
+ * READ QUESTION STATEMENTS THOROUGHLY
+ * Remember edge cases
+ * Think simple (sometimes hardcode scenarios)
+ * Look at Input and Output bounds
+*/
