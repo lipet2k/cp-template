d2fe8c6f6b7bfb1ff56506f2e2d239ad7bcc2af7
Peter Li <lipet2k@Peters-MBP.fios-router.home>
2024-05-11 12:55:02 -0400

    Leetcode template

---
+++ leetcode_template.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <math.h>
+#include <vector>
+#include <math.h>
+#include <set>
+#include <map>
+
+using namespace std;
+
+// typedefs
+typedef long long ll;
+
+typedef vector<int> vi;
+typedef pair<int, int> pi;
+
+// macros
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+
+#define REP(i, a, b) for (int i = a; i <= b; ++i)
+
+#define SQ(a) (a)*(a)
+
+void solve() {
+}
+
+int main() {
+        ios::sync_with_stdio(0);
+        cin.tie(0);
+
+        int n;
+        cin >> n;
+
+        while(--n)
+                solve();
+
+        return 0;
+}
+
+
