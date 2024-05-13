4d25f13f44542d444a19bcc8658bd420b22bac85
Peter Li <lipet2k@Peters-MBP.fios-router.home>
2024-05-13 11:51:07 -0400

    binary search

--- algorithms.cpp
+++ algorithms.cpp
@@ -108,6 +108,29 @@ vector<bool> sieve(int n)
         return is_prime;
 }
 
+int bin_search(int arr[], int target) {
+
+    int left = 0;
+    int right = sizeof(arr) - 1;
+
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        
+        if (arr[mid] == target)
+            return mid;
+
+        if (arr[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    return -1;
+}
+
+
+
+
 int main()
 {
         ios::sync_with_stdio(0);
